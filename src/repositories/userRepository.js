a16b6ab511df15f1fee7c4fde60a4b96c38a18f7
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-06 18:20:35 -0500

    Actualizar importación de base de datos en userRepository.js
    Este commit actualiza la importación de la base de datos en el archivo userRepository.js. Se comenta la línea de código que importa la base de datos y se exporta como un comentario. Esto permite desactivar temporalmente la conexión a la base de datos sin eliminar el código. Estos cambios mejoran la flexibilidad y mantenibilidad del código.

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,16 +1,16 @@
-import UserModel from "../database/db.js";
+// import UserModel from "../database/db.js";
 import { BadRequest } from "../middleware/errors.js";
 
 class UserRepository {
-    #userModel;
+    // #userModel;
     constructor() {
-        this.#userModel = UserModel;
+        // this.#userModel = UserModel;
     }
 
     async createUsers(data) {
         try {
 
-            const results = await this.#userModel.collection('users').insertMany([data]);
+            // const results = await this.#userModel.collection('users').insertMany([data]);
             if (results.acknowledged === false) throw new BadRequest("Error  al crear usuario");
 
             const insertedIds = results.insertedIds;
