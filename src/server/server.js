96b72db1d57048d7d9f46fa391dcf4d593e05a78
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-06 18:20:42 -0500

    Actualizar importación de base de datos en server.js
    Este commit actualiza la importación de la base de datos en el archivo server.js. Se comenta la línea de código que importa la base de datos y se exporta como un comentario. Esto permite desactivar temporalmente la conexión a la base de datos sin eliminar el código. Estos cambios mejoran la flexibilidad y mantenibilidad del código.

--- src/server/server.js
+++ src/server/server.js
@@ -3,12 +3,10 @@ import envalid from "envalid";
 
 import app from "../index.js";
 import  env  from "../config/env.js"
-import db from "../database/db.js";
 
 
 
 const startServer = async () => {
-    await db
     app.listen(env.PORT, () => {
         console.log("")
         console.log(`Server is running on port ${env.PORT}`.bgBlue.black);
