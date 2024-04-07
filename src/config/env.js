da8acbc3721770012802936045e06528b514ff13
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-07 18:01:06 -0500

    Add AWS_BUCKET_NAME configuration to env.js

--- src/config/env.js
+++ src/config/env.js
@@ -9,6 +9,7 @@ const env = envalid.cleanEnv(process.env, {
    DATABASE_NAME: str({ desc: 'pendiente de configurar el nombre de la base de datos' }),
    PORT: port({ desc: 'pendiente configurar el puerto de la aplicacion' }),
    AUTH_URL: str({ desc: 'pendiente de configurar la url de autenticacion' }),
+   AWS_BUCKET_NAME: str({ desc: 'pendiente de configurar el nombre del bucket de aws' }),
 });
 
 export default env;
