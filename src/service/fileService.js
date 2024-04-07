89399e77104be59fd1d2168a32560086c384a781
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-07 11:16:05 -0500

    Refactor fileService.js to include upload middleware for file uploads

--- src/service/fileService.js
+++ src/service/fileService.js
@@ -1,6 +1,7 @@
 import AWS from 'aws-sdk'
 import config from '../config/config.js'
 import path from 'path'
+import upload from '../middleware/upload.js'
 
 const s3 = new AWS.S3()
 
@@ -12,18 +13,14 @@ class FilesService {
 
     async upload(fileName, file) {
         try {
-            const ext = path.extname(fileName);
-    
+            const ext = path.extname(fileName);    
             const uploadParams = {
                 Bucket: config.awsBucketName,
                 Key: `uploads/${Date.now()}${ext}`,
                 Body: file
-            };
-    
-            const uploadResult = await s3.upload(uploadParams).promise();
-    
+            };    
+            const uploadResult = await s3.upload(uploadParams).promise();    
             console.log('UPLOAD RESULT:', uploadResult);
-    
             const key = uploadResult.Key;
             const url = `${config.cloudFrontUrl}/${key}`;
             const location = uploadResult.Location;
