4569fad1c3a3b5129d149af39ab7084602d8e9a1
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-09 17:06:12 -0500

    Add AWS bucket configuration to config.js

--- src/config/config.js
+++ src/config/config.js
@@ -10,12 +10,10 @@ const config = {
    port: process.env.PORT,
    urlMongodb: process.env.DATABASE_URL,
    dataBase: process.env.DATABASE_NAME,
-   awsBucketName:process.env.AWS_BUCKET_NAME
+   awsBucketName: process.env.AWS_BUCKET_NAME,
+   awsBucketRegion: process.env.AWS_BUCKET_REGION,
+   awsBucketAccessKey: process.env.AWS_ACCESS_KEY_ID,
+   awsBucketSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 }
-
-
-
-
-
 
 export default config
