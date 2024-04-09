84f891de8f3fb8a33f844ac538241fd2fe449576
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-08 20:05:27 -0500

    Refactor login method in authService.js to handle user authentication and add login route in authRouter.js

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -38,6 +38,24 @@ function authRouter(app) {
         }
 
     })
+
+    router.post("/login", async (req, res) => {
+        try {
+            const body = req.body;
+            
+            const response = await authServ.login(body);
+            if (!response.success) throw new BadRequest(response.error.message);
+
+            const { user, token } = response;
+            results(res, 200, true, "User login ", {
+                results: user,
+                token,
+            });
+
+        } catch (error) {
+            errorResponse(res, error.message);
+        }
+    })
 }
 
 
