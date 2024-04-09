215030e1775d3076f74d34ec2e6b2d22c13ef15e
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-09 00:51:09 -0500

    Refactor env.js to use cleanEnv function from envalid

--- src/config/env.js
+++ src/config/env.js
@@ -1,10 +1,8 @@
-import envalid from "envalid";
+import { cleanEnv, str, port } from "envalid";
 
 import 'dotenv/config'
-const { str, port } = envalid
 
-
-const env = envalid.cleanEnv(process.env, {
+const env = cleanEnv(process.env, {
    DATABASE_URL: str({ desc: 'pendiente de configurar la url de la base de datos' }),
    DATABASE_NAME: str({ desc: 'pendiente de configurar el nombre de la base de datos' }),
    PORT: port({ desc: 'pendiente configurar el puerto de la aplicacion' }),
