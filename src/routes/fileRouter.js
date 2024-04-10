3fea98d4bc60540db89854b437f4524d86664fff
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-10 11:09:20 -0500

    Refactor fileRouter.js para manejar la descarga de archivos

--- src/routes/fileRouter.js
+++ src/routes/fileRouter.js
@@ -5,6 +5,7 @@ import fileUpload from 'express-fileupload'
 import { Router } from 'express';
 import FilesService from '../service/fileService.js';
 import { errorResponse } from '../helper/response.js';
+import { BadRequest } from '../middleware/errors.js';
 // import { downloadFile, getFile, getFiles, getFileURL, uploadFile } from '../libs/Ejemplostorage.js';
 
 function fileRouter(app) {
@@ -20,10 +21,10 @@ function fileRouter(app) {
         try {
 
             const results = filesServ.uploadMany(req.files)
-            if(!results) {
-                return errorResponse(res, {message: "An error occurred while uploading the file"})
+            if (!results) {
+                return errorResponse(res, { message: "An error occurred while uploading the file" })
             }
-            
+
             res.status(200).json({
                 success: true,
                 message: 'Files uploaded successfully',
@@ -37,6 +38,26 @@ function fileRouter(app) {
 
 
 
+    });
+
+    router.get("/download/:fileName", (req, res) => {
+
+        try {
+
+            const { fileName } = req.params
+            const result = filesServ.download(fileName,res)
+            if(!result.success) throw new BadRequest("Error al descargar el archivo")
+            
+            res.status(200).json({
+                success: true,
+                message: 'File downloaded successfully',
+                // data: result
+            })
+
+        } catch (error) {
+
+            return errorResponse(res, { message: "An error occurred while downloading the fileee" })
+        }
     });
 
 
