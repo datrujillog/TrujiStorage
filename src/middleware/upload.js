b228761f438c8eceff53437caa7e8bb06f02db82
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-09 22:13:56 -0500

    Refactor fileRouter.js to use multer.diskStorage for file uploads

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,11 +1,21 @@
-import multer from "multer"
-const storage = multer.memoryStorage()
+import multer from 'multer';
 
+// Configuración de almacenamiento en disco
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/'); // Reemplaza '/ruta/a/directorio' con la ruta de tu directorio de almacenamiento
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Usa el nombre original del archivo
+  }
+});
+
+// Configuración de multer con almacenamiento en disco
 const upload = multer({
-    storage,
-    limits:{
-        fieldSize:10000000
-    }
-})
+  storage: storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024 // Tamaño máximo del archivo en bytes (10 MB en este ejemplo)
+  }
+});
 
-export default upload
+export default upload;
