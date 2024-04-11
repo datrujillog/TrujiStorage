10da1ceaa661fa93ffacfd5890dd613522fb944a
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-11 17:47:25 -0500

    Refactorizar uso de asyncHandler en index.js para envolver los routers en la función asyncHandler.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,16 +2,16 @@ import express from 'express';
 import authRouter from './authRouter.js';
 import fileRouter from './fileRouter.js';
 import fileEjemploRouter from './fileEjemploRouter.js';
+import { asyncHandler } from '../middleware/handler.js';
 
 const router = express.Router();
-
 // Rutas de autenticación con prefijo '/api/v1/auth'
-router.use('/api/v1/auth', authRouter);
+router.use('/api/v1/auth', asyncHandler(authRouter));
 
 // Rutas de archivos con prefijo '/api/v1/files'
-router.use('/api/v1/files', fileRouter);
+router.use('/api/v1/files', asyncHandler(fileRouter));
 
 // Rutas de archivos de ejemplo con prefijo '/api/v1/file-ejemplo'
-router.use('/api/v1/file-ejemplo', fileEjemploRouter);
+router.use('/api/v1/file-ejemplo', asyncHandler(fileEjemploRouter));
 
 export default router;
