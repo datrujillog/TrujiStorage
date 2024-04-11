43220e3fe1dcf1539a5acf975fe47135e064fb64
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-11 10:00:51 -0500

    Refactorizar servicio de archivos en fileService.js

--- src/service/fileService.js
+++ src/service/fileService.js
@@ -1,93 +1,74 @@
-import { PrismaClient } from "@prisma/client";
+import { BadRequest } from '../middleware/errors.js';
+import fileRepository from '../repositories/fileRepository.js';
 
-import { BadRequest } from '../middleware/errors.js'
-import FileRepository from '../repositories/fileRepository.js'
+import { deleteFiles, downloadFile, uploadFiles } from '../libs/storage.js';
 
-import { deleteFiles, downloadFile, uploadFiles } from '../libs/storage.js'
+class FilesService {
+    static #instance;
 
-class FilesService extends FileRepository {
     constructor() {
-        super()
-        // this.fileModel = new PrismaClient()
+        if (!FilesService.#instance) {
+            FilesService.#instance = this;
+        }
 
+        return FilesService.#instance;
     }
 
     async uploadMany(files) {
-        const results = await uploadFiles(files)
-        // gardar en la base de datos los archivos subidos
+        const results = await uploadFiles(files);
         const promises = results.map(async (result) => {
             if (result.status === 'fulfilled') {
-                return await this.createfileName(result.value)
+                return await fileRepository.createFile(result.value);
             }
-        })
+        });
 
-        const results2 = await Promise.all(promises)
+        const results2 = await Promise.all(promises);
         return {
             success: true,
             message: 'Files uploaded successfully',
-            // data: results2
             data: results2
-        }
+        };
     }
 
-
     async download(fileName, res) {
-
         try {
-
-            const file = await this.findFileById(fileName)
-            if (!file.success) throw new BadRequest(file.error)
+            const file = await fileRepository.findFileByName(fileName);
+            if (!file.success) throw new BadRequest(file.error);
 
             if (file) {
-                return await downloadFile(fileName, res)
+                return await downloadFile(fileName, res);
             }
+
             return {
                 success: false,
                 message: "File not found",
                 res
-            }
-
+            };
         } catch (error) {
             return {
                 success: false,
                 message: error.message
-            }
-
+            };
         }
-
-
-
-
     }
 
-
     async delete(fileName) {
-
         try {
-            const results = await deleteFiles(fileName)
-            if (!results.success) throw new BadRequest(results)
+            const results = await deleteFiles(fileName);
+            if (!results.success) throw new BadRequest(results);
 
-            const deleteFele = await this.deleteFile(results.key)
-            if (!deleteFele.success) throw new BadRequest(deleteFele.error)
+            const deleteFile = await fileRepository.deleteFile(results.key);
+            if (!deleteFile.success) throw new BadRequest(deleteFile.error);
 
             return {
                 success: true,
                 message: "File deleted successfully",
-                deleteFele
-            }
-
-
+                deleteFile
+            };
         } catch (error) {
-            throw new BadRequest(error.message)
-
+            throw new BadRequest(error.message);
         }
     }
-
-
-
-
-
-
 }
 
-export default FilesService
+export default new FilesService();
