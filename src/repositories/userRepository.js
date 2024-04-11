d4b11fa740b31aafa4ec65d6793733359d8ca754
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-11 08:51:48 -0500

    Refactorizar UserRepository para utilizar una única instancia y getClient en userRepository.js

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,17 +1,27 @@
 import { PrismaClient } from "@prisma/client";
-
 import { BadRequest, NotFound } from "../middleware/errors.js";
-
+import getClient from "../libs/db.js";
 
 class UserRepository {
+    static #instance; // Propiedad estática para almacenar la única instancia
+
     #userModel;
+
     constructor() {
-        this.#userModel = new PrismaClient().user;
+        // Verificamos si ya hay una instancia creada
+        if (!UserRepository.#instance) {
+            // Si no hay una instancia, creamos una nueva y la asignamos a la propiedad estática
+            UserRepository.#instance = this;
+            // this.#userModel = new PrismaClient().user;
+            this.#userModel = getClient().user;
+        }
+
+        // Devolvemos la instancia existente
+        return UserRepository.#instance;
     }
 
     async createUsers(data) {
         try {
-
             const user = await this.#userModel.create({
                 data
             });
@@ -19,47 +29,36 @@ class UserRepository {
             return {
                 success: true,
                 user
-            }
-
-
+            };
         } catch (error) {
             console.log(error);
             return { success: false, error: { message: error.message } };
-
         }
-
-
     }
 
     async getByEmail(email) {
-
         try {
+            if (email === undefined) throw new BadRequest("Email is required");
 
-            if(email === undefined) throw new BadRequest("Email is required");
-
-            const resulEmail = await this.#userModel.findUnique({
+            const resultEmail = await this.#userModel.findUnique({
                 where: {
                     email
                 }
             });
 
-            if (!resulEmail) throw new NotFound("User not found");
+            if (!resultEmail) throw new NotFound("User not found");
 
             return {
                 success: true,
-                resulEmail 
-            }
-
+                resultEmail
+            };
         } catch (error) {
-            // return { success: false, error };
-            throw new BadRequest(error);
-
+            // Lanza el error en lugar de devolverlo para mantener el flujo de errores consistente
+            throw error;
         }
     }
-
-
 }
 
-export {
-    UserRepository
-}
+// Exporta una instancia única de UserRepository en lugar de la clase
+export default new UserRepository();
+
