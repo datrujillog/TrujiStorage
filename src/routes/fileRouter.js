7105f099fae08d920cc368b06a6b5af9b46f77e8
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-11 17:30:22 -0500

    Refactorizar manejo de autenticación en fileRouter.js para incluir verificación de token y extracción de datos

--- src/routes/fileRouter.js
+++ src/routes/fileRouter.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import filesService from '../service/fileService.js';
 import uploadFile from '../middleware/upload.js';
 import { BadRequest } from '../middleware/errors.js';
+import { auth } from '../middleware/auth.js';
 import { errorResponse } from '../helper/response.js';
 
 class FileRouter {
@@ -22,6 +23,8 @@ class FileRouter {
         this.router.post("/upload", uploadFile.array('files'), async (req, res) => {
             try {
                 const userId = req.headers.userid;
+                const token = req.cookies.token;
+                await auth(userId, token);
                 const results = await filesService.uploadMany(req.files, userId);
                 if (!results) {
                     return errorResponse(res, { message: "An error occurred while uploading the file" });
@@ -36,6 +39,9 @@ class FileRouter {
         this.router.get("/download/:fileName", async (req, res) => {
             try {
                 const { fileName } = req.params;
+                const userId = req.headers.userid;
+                const token = req.cookies.token;
+                await auth(userId, token);
                 const result = await filesService.download(fileName, res);
                 res.status(200).json({ data: result });
             } catch (error) {
@@ -46,6 +52,9 @@ class FileRouter {
         this.router.delete("/delete/:fileName", async (req, res) => {
             try {
                 const fileName = req.params.fileName;
+                const userId = req.headers.userid;
+                const token = req.cookies.token;
+                await auth(userId, token);
                 const result = await filesService.delete(fileName);
                 const { deleteFele } = result;
                 if (!result.success) throw new BadRequest(deleteFele.error);
