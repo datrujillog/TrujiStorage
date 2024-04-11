e9a3cac2919e4a0fc4a944ba9aa7e3f002e837ba
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-10 20:35:35 -0500

    Agregar función 'deleteFile' para eliminar un archivo en fileRepository.js

--- src/repositories/fileRepository.js
+++ src/repositories/fileRepository.js
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 import { BadRequest, NotFound } from "../middleware/errors.js";
+import upload from "../middleware/upload.js";
 
 
 class FileRepository {
@@ -8,7 +9,7 @@ class FileRepository {
     constructor() {
         this.#fileModel = new PrismaClient().file;
     }
-
+    //! Método para subir archivos falta revisar  el userId 
     async createfileName(data) {
         try {
             const userId = 2
@@ -42,13 +43,14 @@ class FileRepository {
 
         try {
 
-            const file = await this.#fileModel.findUnique({
+            const file = await this.#fileModel.findMany({
                 where: {
                     name: fileName
                 }
             });
 
-            if (!file) throw new NotFound("File not found");
+            if (file.length === 0) throw new NotFound(" File not found");
+
 
             return {
                 success: true,
@@ -57,11 +59,37 @@ class FileRepository {
 
         } catch (error) {
 
-            throw new BadRequest(error);
-            
-            // return { success: false, error: { message: error.message } };
+            return { success: false, error };
         }
     }
+
+
+    async deleteFile(fileName) {
+        try {
+
+            const cleanedFileName = fileName.replace('uploads/', '');
+
+            const file = await this.#fileModel.deleteMany({ where: { name: cleanedFileName } })
+            if (file.count === 0) throw new NotFound("File not found");
+
+            return {
+                success: true,
+                message: 'File deleted successfully'
+            };
+
+        } catch (error) {
+            return {
+                success: false,
+                error: { message: error.message }
+            };
+        }
+    }
+
+
+
+
+
 }
+
 
 export default FileRepository;
