1c7f0084be9dfa11660766ce558ec2119bf62638
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-14 11:43:44 -0500

    Add subscriptionService.js for managing subscriptions

---
+++ src/service/subscriptionService.js
@@ -0,0 +1,44 @@
+
+import Stripe from 'stripe';
+
+import { BadRequest } from '../middleware/errors.js';
+import env from '../config/env.js';
+
+// const stripe = new Stripe(env.STRIPE_PUBLIC_KEY);
+
+// const customer = await stripe.customers.create({
+//   email: 'customer@example.com',
+// });
+
+// console.log(customer.id);
+
+
+class SubscriptionService {
+    static #instance;
+
+    constructor() {
+        if (!SubscriptionService.#instance) {
+            SubscriptionService.#instance = this;
+        }
+
+        return SubscriptionService.#instance;
+    }
+
+    async createSubscription(data) {
+
+        console.log(data)
+
+        return {
+            success: true,
+            subscription: data
+        }
+
+        
+    }
+
+    
+
+    
+}
+
+export default new SubscriptionService();
