f383f63c6eaf0cb0889b3ca98e5d4e762de8c9a4
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-11 13:44:06 -0500

    chore: Comment out unused DATABASE_NAME configuration in env.js

--- src/config/env.js
+++ src/config/env.js
@@ -4,7 +4,7 @@ import 'dotenv/config'
 
 const env = cleanEnv(process.env, {
    DATABASE_URL: str({ desc: 'pendiente de configurar la url de la base de datos' }),
-   DATABASE_NAME: str({ desc: 'pendiente de configurar el nombre de la base de datos' }),
+   // DATABASE_NAME: str({ desc: 'pendiente de configurar el nombre de la base de datos' }),
    PORT: port({ desc: 'pendiente configurar el puerto de la aplicacion' }),
    AUTH_URL: str({ desc: 'pendiente de configurar la url de autenticacion' }),
    JWT_SECRET: str({ desc: 'pendiente de configurar el secreto del token' }),
